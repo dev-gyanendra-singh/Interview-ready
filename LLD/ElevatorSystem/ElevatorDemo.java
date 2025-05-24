d06898356a327cc576851e0aa17893ab7541c6c7
Gyanendra Singh <71885051+dev-gyanendra-singh@users.noreply.github.com>
2025-05-24 19:41:52 +0530

    Add files via upload

---
+++ LLD/ElevatorSystem/ElevatorDemo.java
@@ -0,0 +1,16 @@
+package LLD.ElevatorSystem;
+
+import java.util.Random;
+
+public class ElevatorDemo {
+    public static void main(String[] args) {
+        ElevatorManager manager = new ElevatorManager(2);
+        boolean flag = false;
+        Random random = new Random();
+        for (int i = 1; i <= 500; i++) {
+            manager.requestElevator(random.nextInt(i), random.nextInt(i));
+            flag = !flag;
+        }
+
+    }
+}
